9bb810de3102a49d09bfdafdcb6cf711639b2842
João Paulo Cerquinho Cajueiro <joaopaulo.cerquinhocajueiro@gmail.com>
2018-05-01 14:13:25 -0300

    Alterado o desenho das entradas

--- public/Bit.js
+++ public/Bit.js
@@ -10,6 +10,20 @@ function Bit(name,pos,type){
         push();
         translate(this.pos.x*colSize,this.pos.y*linSize); // Move to the position
         scale(colSize,linSize); // Scale to the actual element size
+        if(this.type=="input"){
+            this.drawInput()
+        } else {
+            this.drawOutput()
+        }
+        noStroke();
+        fill(0);
+        textSize(0.3); 
+        textAlign(CENTER,BOTTOM);
+        text(this.name,0,0.5)
+        pop();
+    }
+
+    this.drawOutput = function(){
         stroke(0);
         strokeWeight(0.01);
         if(this.value == 0){
@@ -17,13 +31,22 @@ function Bit(name,pos,type){
         } else {
             fill(150,0,0);
         }
-        rect(-0.2,-0.2,0.4,0.4,0.1);
-        noStroke();
-        fill(0);
-        textSize(0.3); 
-        textAlign(CENTER,BOTTOM);
-        text(this.name,0,0.5)
-        pop();
+        rect(-0.2,-0.2,0.4,0.4,0.15);
+    }
+
+    this.drawInput = function(){
+        stroke(0);
+        strokeWeight(0.01);
+        noFill();
+        rect(-0.3,-0.2,0.6,0.4,0.15);
+        if(this.value == 0){
+            fill(0,0,150);
+            pos=-0.3
+        } else {
+            fill(150,0,0);
+            pos=0
+        }
+        rect(pos,-0.2,0.3,0.4,0.15);
     }
 
     this.mouseIsOver = function() {
