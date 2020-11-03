9ad26ee6ab1ba56520b2c457e4e878a9202c48b8
joaopaulo-cerquinhocajueiro <joaopaulo@ufpe.br>
2020-11-02 22:14:20 -0300

    Primeira versao do egginator. Nao funcional

---
+++ egginator/egginator.js
@@ -0,0 +1,453 @@
+class Ovo {
+    constructor(svg){
+        this.svg = svg;
+        this.estado = 0;
+        this.aniCounterMax = 2;
+        this.aniCounter = 0;
+        this.ovos = [
+            this.svg.contentDocument.getElementById("ovoinicial"),
+            this.svg.contentDocument.getElementById("ovo1"),
+            this.svg.contentDocument.getElementById("ovo2"),
+            this.svg.contentDocument.getElementById("ovo3")];
+        this.crack = this.svg.contentDocument.getElementById("rachadura");
+        for(let i = 1;i<4;i++){
+            this.ovos[i].style.visibility = "hidden";
+        }
+        this.crack.style.visibility = "hidden";
+    }
+    update(){
+        switch(this.estado){
+            case 0: if (sistema.portinhola.aberto){
+                this.estado = 1;
+                }
+                break;
+            case 1: if(++this.aniCounter>=this.aniCounterMax){
+                this.estado = 2;
+                this.aniCounter = 0;
+                }
+                break;
+            case 2: if(++this.aniCounter>=this.aniCounterMax){
+                if(sistema.panela.h >= sistema.panela.hmax){
+                    this.estado = 3;
+                } else {
+                    this.estado = 4;
+                }
+                this.aniCounter = 0;
+                }
+                break;
+            default: break;
+        }
+        for(let i = 0;i<4;i++){
+            if(this.estado == i){
+                this.ovos[i].style.visibility="visible";
+            } else {
+                this.ovos[i].style.visibility="hidden";
+            }
+        }
+        if(this.estado == 4){
+            this.ovos[3].style.visibility="visible";
+            this.crack.style.visibility="visible";
+        }
+    }
+}
+
+class Panela {
+    constructor(svg){
+        this.svg = svg;
+//        console.log(this.svg);
+        this.pan = this.svg.contentDocument.getElementById('panela');
+        //console.log(this.svg);
+        this.aguaBaixo = this.svg.contentDocument.getElementById('aguaBaixo');
+        this.aguaTopo = this.svg.contentDocument.getElementById('aguaTopo');
+        this.y0 = this.aguaTopo.y.baseVal.value;
+        this.hmax = this.aguaTopo.height.baseVal.value;
+        this.h = 0;
+        this.present = false;
+        this.hide();
+        //this.element = svg.contentDocument.getElementById(elementId);
+    }
+    setH = (newH)=>{
+        this.aguaTopo.height.baseVal.value = newH;
+        this.aguaTopo.y.baseVal.value = this.y0+this.hmax - newH;
+    }
+    increase = (amount)=>{
+        // console.log(this);
+        if (this.h>this.hmax-amount){
+            this.h = this.hmax;
+        } else {
+            this.h = this.h + amount;
+        }
+        this.setH(this.h);
+    }
+    decrease = (amount)=>{
+        // console.log(this,"decrease")
+        if (this.h < amount){
+            this.h = 0;
+        } else {
+            this.h = this.h - amount;
+        }
+        this.setH(this.h);
+    }
+    show = ()=>{
+        this.pan.style.visibility = "visible";
+        if(this.h>0){
+            this.aguaBaixo.style.visibility = "visible";
+            this.aguaTopo.style.visibility = "visible";
+        }
+        this.present = true;
+    }
+    hide = ()=>{
+        this.pan.style.visibility = "hidden";
+        this.aguaBaixo.style.visibility = "hidden";
+        this.aguaTopo.style.visibility = "hidden";
+        this.h = 0;
+        this.setH(this.h);
+        this.present = false;
+    }
+}
+
+class Valve {
+    constructor(svg,elementId){
+        this.svg = svg;
+        this.element = svg.contentDocument.getElementById(elementId);
+        console.log(this, this.svg,this.element);
+        this.value = 0;
+        this.type = 'output';
+        this.name = elementId;
+    }
+    update = ()=>{
+        if (this.value == 1){
+            for(let x of this.element.children){
+                if(x.childElementCount>0){
+                    for(let y of x.children){
+                        y.style.stroke = "rgb(0,255,0)";
+                    }
+                }
+                x.style.stroke = "rgb(0,255,0)";
+            }
+        } else {
+            for(let x of this.element.children){
+                if(x.childElementCount>0){
+                    for(let y of x.children){
+                        y.style.stroke = "rgb(255,0,0)";
+                    }
+                }
+                x.style.stroke = "rgb(255,0,0)";
+            }
+        }
+    }
+    json = ()=>{
+        var returnValue = {};
+        returnValue['type'] = this.type;
+        returnValue['name'] = this.name;
+        return returnValue;
+    }
+}
+
+class Selector {
+    constructor(svg){
+        this.svg = svg;
+        this.PO = svg.contentDocument.getElementById("PO");
+        this.POfechado = svg.contentDocument.getElementById("POfechado");
+        this.POaberto = svg.contentDocument.getElementById("POaberto");
+        this.POaberto.style.visibility = "hidden";
+        this.aberto = false;
+        this.value = 0;
+        this.type = 'output';
+        this.name = 'PO';
+    }
+    update = ()=>{
+        this.aberto = this.value == 1;
+        if (this.aberto){
+            this.POaberto.style.visibility = "visible";
+            this.POfechado.style.visibility = "hidden";
+            this.PO.style.stroke = "rgb(0,255,0)";
+        } else {
+            this.POaberto.style.visibility = "hidden";
+            this.POfechado.style.visibility = "visible";
+            this.PO.style.stroke = "rgb(255,0,0)";
+        }
+    }
+    json = ()=>{
+        var returnValue = {};
+        returnValue['type'] = this.type;
+        returnValue['name'] = this.name;
+        return returnValue;
+    }
+}
+
+class Sensor {
+    constructor(svg,elementId){
+        this.svg = svg;
+        this.element = svg.contentDocument.getElementById(elementId);
+        this.out = false;
+        this.value = 0;
+        this.type = 'input';
+        this.name = elementId;
+    }
+    update = ()=>{
+        switch(this.name){
+            case "SN": this.value = sistema.panela.h > sistema.panela.hmax?1:0;
+                break;
+            case "SPP": this.value = sistema.panela.present?1:0;
+                break;
+            case "SG": this.value = sistema.valveGas.value;
+        }
+        if (this.value == 1){
+            for(let x of this.element.children){
+                if(x.childElementCount>0){
+                    for(let y of x.children){
+                        y.style.stroke = "rgb(0,255,0)";
+                    }
+                }
+                x.style.stroke = "rgb(0,255,0)";
+            }
+        } else {
+            for(let x of this.element.children){
+                if(x.childElementCount>0){
+                    for(let y of x.children){
+                        y.style.stroke = "rgb(255,0,0)";
+                    }
+                }
+                x.style.stroke = "rgb(255,0,0)";
+            }
+        }
+    }
+    json = ()=>{
+        var returnValue = {};
+        returnValue['type'] = this.type;
+        returnValue['name'] = this.name;
+        return returnValue;
+    }
+}
+
+class SensorTemp {
+    constructor(svg){
+        this.svg = svg;
+        this.element = svg.contentDocument.getElementById("temperature");
+        this.out = false;
+        this.value = 0;
+        this.temperature = 20.0;
+        this.y0 = this.element.y.baseVal.value;
+        this.hmax = this.element.height.baseVal.value;
+        this.h = 0;
+        this.type = 'input';
+        this.name = "ST";
+    }
+    update = ()=>{
+        if(sistema.panela.present){
+            if (sistema.fogo.visibility == "visible"){
+                this.temperature += 0.2*(100.0-this.temperature);
+            } else {
+                this.temperature -= 0.2*(this.temperature-20.0);
+            }
+        }  else {
+            this.temperature = 20;
+        }
+        let h = (this.temperatura-20.0)*this.hmax/80.0;
+        this.element.height.baseVal.value = h;
+        this.element.y.baseVal.value = this.y0 + this.hmax - h;
+
+
+        this.value = this.temperature>80? 1: 0;
+        if (this.value == 1){
+            this.element.style.fill = "rgb(0,255,0)";
+        } else {
+            this.element.style.fill = "rgb(255,0,0)";
+        }
+    }
+    json = ()=>{
+        var returnValue = {};
+        returnValue['type'] = this.type;
+        returnValue['name'] = this.name;
+        return returnValue;
+    }
+}
+
+class Centelha{
+    constructor(svg){
+        this.svg = svg;
+        this.element = svg.contentDocument.getElementById("centelha");
+        console.log(this, this.svg,this.element);
+        this.value = 0;
+        this.type = 'output';
+        this.name = 'centelha';
+    }
+    update = ()=>{
+        if (this.value == 1){
+            for(let x of this.element.children){
+                if(x.childElementCount>0){
+                    for(let y of x.children){
+                        y.style.stroke = "rgb(0,255,0)";
+                    }
+                }
+                x.style.stroke = "rgb(0,255,0)";
+            }
+        } else {
+            for(let x of this.element.children){
+                if(x.childElementCount>0){
+                    for(let y of x.children){
+                        y.style.stroke = "rgb(255,0,0)";
+                    }
+                }
+                x.style.stroke = "rgb(255,0,0)";
+            }
+        }
+    }
+    json = ()=>{
+        var returnValue = {};
+        returnValue['type'] = this.type;
+        returnValue['name'] = this.name;
+        return returnValue;
+    }
+}
+
+class Egginator{
+    constructor(svg,memories,counters){
+        //console.log(this);
+        //this.svg = svg.contentDocument;
+        //this.element = this.svg.getElementById('svg1');
+        this.svg = svg;
+        this.panela = new Panela(this.svg);
+
+        this.valveAgua = new Valve(this.svg,"VA");
+        this.valveGas = new Valve(this.svg,"VG");
+
+        this.centelha = new Centelha(this.svg);
+
+        this.portinhola = new Selector(this.svg);
+
+        this.SN = new Sensor(this.svg,"SN");
+        this.SPP = new Sensor(this.svg,"SPP");
+        this.SG = new Sensor(this.svg,"SG");
+
+        this.ST = new SensorTemp(this.svg);
+    
+        this.ovo = new Ovo(this.svg);
+        
+        this.aguaSaindo = this.svg.contentDocument.getElementById("aguaSaindo");
+        this.aguaSaindo.style.visibility = "hidden";
+
+        this.fogo = this.svg.contentDocument.getElementById("fogo");
+        this.fogo.style.visibility = "hidden";
+
+        this.inputs =  [this.SPP, this.SN, this.SG, this.ST];
+        this.outputs = [this.valveAgua,this.valveGas,this.centelha,this.portinhola];
+        this.coisos = this.inputs.concat(this.outputs);
+
+        //this.create(null,this.memories,this.counters);
+                
+    }
+
+    create(inputs, memories, counters){
+        //this.svg.clear();
+        var posY = 0;
+        this.svg.text('inputs').move(30,posY).font({
+            family:   'Helvetica'
+        , size:     20
+        , anchor:   'middle'
+        });
+            posY+=distanceY+20;
+        inputs.forEach(input => {
+            this.coisos.push(new Bit(input,10,posY,"input",this.svg));
+            posY +=distanceY;
+        });
+        
+        posY = 0;
+        this.svg.text('mem').move(105,posY).font({
+            family:   'Helvetica'
+        , size:     20
+        , anchor:   'middle'
+        });
+        posY+=distanceY+20;
+        
+        memories.forEach(memory => {
+            this.coisos.push(new Bit(memory,85,posY,"memory",this.svg));
+            posY +=distanceY;
+        });
+        
+        posY = 0;
+        this.svg.text('counters').move(280,posY).font({
+            family:   'Helvetica'
+        , size:     20
+        , anchor:   'middle'
+        });
+        posY+=distanceY+20;
+        counters.forEach(counter => {
+            this.coisos.push(new Value(counter,260,posY,"counter",this.svg));
+            posY +=distanceY*1.5;
+        });
+
+        this.coisos.forEach(item => {
+            item.draw();
+        });
+    }
+
+    reset(){
+        this.PO.value = 0;
+        this.PO.update();
+        this.ovo.estado = 0;
+        this.ovo.update();
+        this.panela.present = true;
+        this.panela.update();
+        this.valveAgua.value = 0;
+        this.valveAgua.update();
+        this.valveGas.value = 0;
+        this.valveGas.update();
+        this.fogo.style.visibility = "hidden";
+        this.SPP.update();
+        this.SG.update();
+        this.SN.update();
+        this.ST.temperature = 20;
+        this.ST.update();
+    }
+
+    simulate = ()=>{
+
+        for(let x of this.outputs){
+            x.update();
+        }
+
+        this.panela.update();
+        this.ovo.update();
+        if(this.valveGas.value == 0){
+            this.fogo.style.visibility = "hidden";
+        } else {
+            if(this.centelha.value == 1){
+                this.fogo.style.visibility = "visible";
+            }
+        }
+        for(let x of this.inputs){
+            x.update();
+        }
+    }
+}
+
+//
+
+
+// window.addEventListener("load", function() {
+
+//     document.getElementById("bValve").addEventListener("click",function(evt){
+//         if(valve.on){
+//             valve.on = false;
+//             evt.srcElement.value = "Válvula desligada";
+//         } else {
+//             valve.on = true;
+//             evt.srcElement.value = "Válvula ligada";
+//         }
+//     });
+
+//     document.getElementById("bSel").addEventListener("click",function(evt){
+//         if(selector.left){
+//             selector.left = false;
+//             evt.srcElement.value = "Direita";
+//         } else {
+//             selector.left = true;
+//             evt.srcElement.value = "Esquerda";
+//         }
+//     });
+
+//     setInterval(simulate,100);
+               
+// });
---
+++ egginator/sketch.js
@@ -0,0 +1,295 @@
+"use strict"
+
+var horz = 9, vert = 7;
+var width = 100*horz;
+var height = 100*vert;
+var sistema;
+var sistemaIntervalId;
+
+var toolBar,elementTables,elementTable,io;
+var buttonErase, buttonSave, buttonSimulate, inputFile;
+var selectedTable = 1;
+var simulating = false;
+
+var globalValues = {};
+var setPoints = {};
+
+var svgToolbar;
+var svgTable;
+var svgIO;
+var simSvg;
+
+var memories = ["m0", "m1", "m2", "m3", "m4", "m5", "m6", "m7", "m8", "m9"];
+var counters = ["c0", "c1", "c2", "c3", "c4", "c5"];
+
+window.addEventListener("load", function() {
+    svgToolbar = SVG('toolbar').size('100%', '100%').viewbox(0,0,360,700);
+    svgTable = SVG('table').size('100%', '100%').viewbox(-20,-20,width+40,height+60);
+    svgIO = SVG('io').size('100%', '100%').viewbox(0,0,600,700);
+
+    toolBar = new ToolBar(svgToolbar);
+    io = new IOView(svgIO, [], memories, [], counters);
+    // sistema = new VaiEVolta(svgSim, memories, counters);
+    simSvg = document.getElementById("sistema");
+
+    sistema = new Egginator(simSvg,memories,counters);  
+    // sistemaIntervalId = window.setInterval(sistema.simulate,100);
+    elementTables = [new ElementTable(svgTable, horz, vert, sistema.coisos.concat(io.coisos))];
+    elementTable = elementTables[selectedTable-1];
+
+    buttonErase = document.getElementById('eraseButton');
+    buttonErase.addEventListener('click',eraseAll);
+
+    buttonSave = document.getElementById('saveButton');
+    buttonSave.addEventListener('click',saveCode);
+
+    // buttonExport = document.getElementById('exportButton');
+    // buttonExport.addEventListener('click',exportCode);
+
+    buttonSimulate = document.getElementById('simulateButton');
+    buttonSimulate.addEventListener('click',simulate);
+
+    // Sempre que mudar o arquivo, carrega o novo
+    inputFile = document.getElementById('inputFile');
+    inputFile.addEventListener('change', handleFileSelect, false);
+});
+
+
+// var buttonInputs = [];
+// var dispMemories = [];
+// var dispOutputs = [];
+
+function indexFromXY(x,y){
+    return y*horz+x;
+}
+
+function renameInTable(elementTable,oldName,newName){
+  for(var i=0; i<elementTable.table.length; i++){
+    if(elementTable.table[i].name == oldName){
+      elementTable.table[i].name = newName;
+    }
+  }
+  for(var i=0; i<elementTable.contactSelector.options.length; i++){
+    if(elementTable.contactSelector.options[i].text == oldName){
+      elementTable.contactSelector.options[i].text = newName;
+      elementTable.contactSelector.options[i].value = newName;
+    }
+  }
+  for(var i=0; i<elementTable.coilSelector.options.length; i++){
+    if(elementTable.coilSelector.options[i].text == oldName){
+      elementTable.coilSelector.options[i].text = newName;
+      elementTable.coilSelector.options[i].value = newName;
+    }
+  }
+  for(var i=0; i<elementTable.counterSelector.options.length; i++){
+    if(elementTable.counterSelector.options[i].text == oldName){
+      elementTable.counterSelector.options[i].text = newName;
+      elementTable.counterSelector.options[i].value = newName;
+    }
+  }
+  
+}
+
+function renameInView(ioView,oldName,newName){
+  var index = ioView.inputs.findIndex(name =>{
+    return name == oldName;
+  });
+  if (index > -1){
+    inputs[index] = newName;
+  }
+  var index = ioView.memories.findIndex(name =>{
+    return name == oldName;
+  });
+  if (index > -1){
+    memories[index] = newName;
+  }
+  var index = ioView.outputs.findIndex(name =>{
+    return name == oldName;
+  });
+  if (index > -1){
+    outputs[index] = newName;
+  }
+  var index = ioView.counters.findIndex(name =>{
+    return name == oldName;
+  });
+  if (index > -1){
+    ioView.counters[index] = newName;
+  }
+  
+}
+
+function isNewName(ioList,name){
+  return ioList.filter(element =>{
+    return element.name == name;
+  }).length == 0;
+}
+
+function changeElementName(oldName,newName){
+  renameInView(sim,oldName,newName);
+  renameInTable(elementTable,oldName,newName);
+}
+
+function choose(choices) {
+    var index = Math.floor(Math.random() * choices.length);
+    return choices[index];
+  }
+
+
+function resize(width,height) {
+//    if (width < 200) {
+//        horz = floor(width/20);
+//    } else {
+//        horz = 10;
+//    }
+//    if (height < 140) {
+//        vert = floor(height/20);
+//    } else {
+//        vert = 7;
+//    }
+    colSize = floor(0.9*width / (1.8*horz));
+    linSize = floor(0.9*height / (1.1*vert));
+    if (colSize < linSize) {
+        linSize = colSize;
+//        vert = floor(height/linSize);
+    } else {
+        colSize = linSize;
+//        horz = floor(width/colSize);        
+    }
+//    console.log("colSize: "+colSize);
+//    console.log("linSize: "+linSize);
+}
+
+
+
+function eraseAll() {
+    elementTable.eraseAll();
+}
+
+
+function saveCode(){
+    var filename = "teste";
+    var codes = "[" + elementTables.map(table => {return table.jsonTable()}).join(", ") + "]";
+    var variables = elementTables[0].jsonVar();
+    var blob = new Blob(['{"codes":' + codes + ', "variables":' + variables +'}'], {type: "text/json;charset=utf-8"});
+    saveAs(blob, filename+".json");
+ //   console.log(elementTable.json())
+}
+
+function exportCode(){
+  var filename = "teste";
+  jsld2ard(elementTable.json(),code=>{
+    var blob = new Blob([code], {type: "text/text;charset=utf-8"});
+    saveAs(blob, filename+".ino");
+  });
+//   console.log(elementTable.json())
+}
+
+function simulate(e){
+    simulating = !simulating;
+    elementTables.forEach(elementTable => {
+      elementTable.simulating = simulating;      
+    });
+    var tableDiv = document.getElementById("tables");
+    var simDiv = document.getElementById("sim");
+    var toolbarDiv = document.getElementById("toolbar");
+    var simButton = e.target;
+    if(simulating){
+        simButton.style.backgroundColor = "#4C50AF";
+        simButton.innerHTML = "Stop simulation";
+        // simDiv.style.display = 'flex';
+        simDiv.style.width = '50%';
+        tableDiv.style.width = '50%';
+        toolbarDiv.style.display = 'none';
+
+        sistemaIntervalId = setInterval(sistema.simulate,10);
+    } else {
+        simButton.style.backgroundColor = "#4CAF50";
+        simButton.innerHTML = "Simulate";
+        //simDiv.style.display = 'none';
+        simDiv.style.width = '25%';
+        toolbarDiv.style.display = 'flex';
+        toolbarDiv.style.width = '25%';
+        tableDiv.style.width = '70%';
+
+        clearInterval(sistemaIntervalId);
+        sistema.reset();
+    }
+    //console.log(elementTable.simulating);
+}
+
+function handleFileSelect(evt) { // always when selecting a new file
+  var files = evt.target.files; // get the array with the file (there is only one)
+  var f = files[0]; // select the first (and only) file
+
+  var reader = new FileReader(); // a reader
+
+  // Closure to capture the file information.
+  reader.onload = (function(theFile) {
+    return function(e) {
+      var codeObject = JSON.parse(e.target.result);
+      elementTables = [];
+      //io.writeJson(codeObject);
+      for(var i=tableTabs.children.length-2;i>-1;i--){
+        tableTabs.removeChild(tableTabs.children[i]);
+      }
+      for(var i = 0;i<codeObject.codes.length;i++){
+        addTable(null);
+        elementTable.writeJson(codeObject.codes[i]);
+        elementTable.ioElements = sistema.coisos.concat(io.coisos);
+      }
+    };
+  })(f);
+
+  // Read in the image file as a data URL.
+  reader.readAsText(f);
+}
+
+// Tab control
+
+function openTable(event,table){
+  var tableTabs = document.getElementById("tableTabs");
+  console.log("Abre tabela "+table);
+  tableTabs.children[selectedTable-1].classList.remove("selected");
+  selectedTable = table;
+  elementTable.hide();
+  elementTable = elementTables[selectedTable-1];
+  elementTable.show();
+  tableTabs.children[selectedTable-1].classList.add("selected");
+}
+
+function addTable(event){
+  var tableTabsLength = tableTabs.children.length;
+  var newButton = document.createElement("button");
+  elementTables.push(new ElementTable(svgTable, horz, vert, sistema.coisos.concat(io.coisos)));
+  elementTable.hide();
+  elementTable = elementTables[elementTables.length-1];
+  elementTable.show();
+  newButton.className += "selected";
+  newButton.innerHTML = tableTabsLength;
+  newButton.onclick = (event => {openTable(event,tableTabsLength)});
+  try {
+     tableTabs.children[selectedTable-1].classList.remove("selected");
+  } catch (error){
+    if(error instanceof TypeError){
+
+    }else {
+      logMyErrors(error);
+    }
+  }
+  selectedTable = tableTabsLength;
+  tableTabs.insertBefore(newButton,tableTabs.lastChild.previousSibling);
+}
+
+function reset(event){
+  sistema.reset();
+}
+
+function togglePan(e){
+  if(sistema.panela.present){
+    sistema.panela.hide();
+    e.srcElement.innerHTML = "Coloca panela";
+  } else {
+    sistema.panela.show();
+    e.srcElement.innerHTML = "Retira panela";
+  }
+}
